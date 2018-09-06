5470d2c644dcea4393914427799f71a54e4ffd9e
ahmednet <ahmednet03@gmail.com>
2018-09-06 22:49:15 +0200

    interval

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,10 @@
-import { fromEvent, from, Observable } from 'rxjs';
+import { fromEvent, from, Observable, interval } from 'rxjs';
+//import { take } from 'rxjs/operators';
 
 // start code 
 class Wordbeater
 {
+	//declare type of item
 	words:any[]
 	currentWord:any
 	time:number
@@ -25,30 +27,31 @@ class Wordbeater
 		this.isPlaying = true;
 		this.score = 0;
 	}
+	// the run method
 	init():void
 	{
 		this.wordPrint();
 		fromEvent(this.wordInputDom, 'input')
-			.subscribe((e: any) => {
-				this.startMatch(e.target.value);
-			})
-		setInterval(()=> {
-			requestAnimationFrame(() => {
-				this.countDown()
-			})
-		}, 1000)
-		setInterval(() => {
-			requestAnimationFrame(()=> {
-				this.checkStatus();
-			})
-		}, 50)
+		.subscribe((e: any) => {
+			this.startMatch(e.target.value);
+		})
+		interval(1000)
+		.subscribe((x: number) => {
+			this.countDown()
+		});
+		interval(50)
+		.subscribe((x: number) => {
+			this.checkStatus()
+		});
 	}
+	//print new word and score to screen
 	wordPrint():void
 	{
 		this.currentWord = this.words[Math.floor(Math.random() * this.words.length)]
 		this.wordDom.textContent = this.currentWord;
 		this.scoreDom.textContent = `${this.score}`;
 	}
+	// count down method
 	countDown():void
 	{
 		if(this.time > 0)
@@ -59,6 +62,7 @@ class Wordbeater
 			this.isPlaying = false
 		}
 	}
+	// print time to screen
 	timeLeftPrint():void
 	{
 		this.timeLeftDom.textContent = `${this.time}`;
@@ -69,6 +73,7 @@ class Wordbeater
 			})
 		}, 300)
 	}
+	//check method
 	checkStatus():void
 	{
 		if(!this.isPlaying && this.time === 0)
@@ -77,6 +82,7 @@ class Wordbeater
 			this.score = 0;
 		}
 	}
+	// input match
 	startMatch(val:any):void
 	{
 		if(val === this.currentWord)
